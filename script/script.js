3368b381b903d7d1c7b6246f6bafcddee4a7232f
BTran418 <btran418@gmail.com>
2019-03-26 16:11:39 -0600

    Update 4

--- script/script.js
+++ script/script.js
@@ -1,15 +1,37 @@
 "use strict";
-const jsonFile;
+let jsonFile = null;
 
-window.onload = function(){
+window.onload = function () {
+    //Get recipe JSON file
     let request = new XMLHttpRequest();
-    jsonFile = 
+    request.open('GET', 'script/recipes.json');
+    request.responseType = 'json';
+    request.send();
+    request.onload = function () {
+        jsonFile = request.response;
+        console.log(jsonFile["recipes"][0]);
+        recipeManager();
+    }
 }
 
-function searchByString(string){
+function searchByString(string) {
 
 }
 
-function searchByTags(tags){
-    
+function searchByTags(tags) {
+
+}
+
+
+function recipeManager() {
+    console.log("Manager");
+    let author = document.getElementById("recipe_author");
+    let ingredients = document.getElementById("recipe_ingredients");
+    let skills = document.getElementById("recipe_skills");
+    let body = document.getElementById("recipe_body");
+
+    author.innerText = jsonFile["recipes"][0].author;
+    ingredients.innerText = jsonFile["recipes"][0].ingredients;
+    skills.innerText = jsonFile["recipes"][0].skills;
+    body.innerText = jsonFile["recipes"][0].bodies[0];
 }
