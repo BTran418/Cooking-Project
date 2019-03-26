42c231aa6b24ff28737e0b82d04cc0da3c3c0382
BTran418 <btran418@gmail.com>
2019-03-26 16:42:24 -0600

    update 5

--- script/script.js
+++ script/script.js
@@ -9,7 +9,9 @@ window.onload = function () {
     request.send();
     request.onload = function () {
         jsonFile = request.response;
-        console.log(jsonFile["recipes"][0]);
+        // console.log(jsonFile["recipes"][0]);
+        // console.log(jsonFile["recipes"][2]["substitutes"]["1"][0]);
+        console.log(jsonFile["recipes"][0].ingredients);
         recipeManager();
     }
 }
@@ -24,14 +26,26 @@ function searchByTags(tags) {
 
 
 function recipeManager() {
-    console.log("Manager");
     let author = document.getElementById("recipe_author");
     let ingredients = document.getElementById("recipe_ingredients");
     let skills = document.getElementById("recipe_skills");
     let body = document.getElementById("recipe_body");
 
     author.innerText = jsonFile["recipes"][0].author;
-    ingredients.innerText = jsonFile["recipes"][0].ingredients;
+    
+    for(let i = 0; i < jsonFile["recipes"][0].ingredients.length; i++){
+
+        ingredients.innerHTML += jsonFile["recipes"][0].ingredients[i] + "<br>";
+    }
+    ingredients.innerHTML += "<br>"
+
     skills.innerText = jsonFile["recipes"][0].skills;
     body.innerText = jsonFile["recipes"][0].bodies[0];
 }
+
+function nextBody(){
+    let body = document.getElementById("recipe_body");
+    body.innerText = jsonFile["recipes"][0].bodies[current+1];
+    body.setAttribute("data-current-part", current+1);
+}
+
