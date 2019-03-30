0a2f713459799579e763b5e252cf163893c7c923
Vinny <viniciusgoula@gmail.com>
2019-03-30 12:23:06 -0600

    1

--- script/script.js
+++ script/script.js
@@ -12,10 +12,6 @@ window.onload = function () {
     request.send();
     request.onload = function () {
         jsonObjects = request.response; //Create recipe objects from a JSON file.
-        // console.log(jsonFile["recipes"][0]);
-        // console.log(jsonFile["recipes"][2]["substitutes"]["1"][0]);
-        // console.log(jsonObjects);
-        // recipeManager();
         populateRecipeSite(targetRecipeID);
     }
 }
@@ -28,46 +24,6 @@ function searchByTags(tags) {
 
 }
 
-//Populates the current recipe page.
-// function recipeManager() {
-//     let author = document.getElementById("recipe_author");
-//     let ingredients = document.getElementById("recipe_ingredients");
-//     let skills = document.getElementById("recipe_skills");
-//     let body = document.getElementById("recipe_body");
-
-//     author.innerText = jsonObjects["recipes"][0].author;
-
-//     for(let i = 0; i < jsonObjects["recipes"][0].ingredients.length; i++){
-
-//         ingredients.innerHTML += jsonObjects["recipes"][0].ingredients[i] + "<br>";
-//     }
-//     ingredients.innerHTML += "<br>"
-
-//     skills.innerText = jsonObjects["recipes"][0].skills;
-//     body.innerText = jsonObjects["recipes"][0].bodies[0];
-// }
-
-//TODO: Delete this after maybe?
-// /**
-//  * Populates with the next body.
-//  */
-// function nextBody() {
-//     let body = document.getElementById("recipe_body");
-//     let current = parseInt(body.getAttribute("data-current-part"));
-//     body.innerText = jsonObjects["recipes"][0].bodies[current + 1];
-//     body.setAttribute("data-current-part", current + 1);
-
-//     checkButtonsState(current);
-// }
-
-// function previousBody() {
-//     let body = document.getElementById("recipe_body");
-//     let current = parseInt(body.getAttribute("data-current-part"));
-//     body.innerText = jsonObjects["recipes"][0].bodies[current - 1];
-//     body.setAttribute("data-current-part", current - 1);
-
-//     checkButtonsState(current);
-// }
 
 /**
  * Returns specific recipe object.
@@ -175,6 +131,7 @@ function populateRecipeSite(id) {
 
 function prevButtonClicked(element) {
     let currStep = parseInt(element.getAttribute("data-current-step"));
+    //Check if there is more before  the current one    
     if (currStep - 1 <= -1) {
         return;
     }
@@ -214,6 +171,7 @@ function prevButtonClicked(element) {
 
 function nextButtonClicked(element) {
     let currStep = parseInt(element.getAttribute("data-current-step"));
+    //Check if there is more after the current one
     if (currStep + 1 >= getRecipeById(targetRecipeID).bodies.length) {
         return;
     }
