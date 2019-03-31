ea03414fbbb800f5695c6c684a79439671b9a5d7
BTran418 <btran418@gmail.com>
2019-03-31 16:09:45 -0600

    Update

--- script/index_script.js
+++ script/index_script.js
@@ -1,3 +1,4 @@
+'use strict';
 /**
  * This script is to be added to the index page only.
  */
@@ -27,17 +28,49 @@ function populateTrending(){
 
     //Selects 4 random recipes
     for(let i = 0; i < 4; i++){
+        let recipe = jsonObjects[getRandomInt(jsonObjects.length)];
+        //TODO: Wait for more than 4 recipes
+        // while (findObjectInArray(chosenRecipes, recipe) == -1){
+        //     recipe = jsonObjects[getRandomInt(jsonObjects.length)];
+        // }
+
         chosenRecipes.push(jsonObjects[getRandomInt(jsonObjects.length)]);
     }
+    console.log(chosenRecipes)
+
     //Get all video containers
     let videoContainers = document.getElementsByClassName('a_video');
-
     for(let i = 0; i < videoContainers.length; i++){
         //Set name
-        videoContainers[0].getElementsByClassName('recipe_tile')[0].children[0].innerText = jsonObjects[i].title;
+        videoContainers[i].children[0].children[0].innerText = chosenRecipes[i].title;
+        console.log(videoContainers[i].children[1].children[0].children);
+
+        //Set media
+        let mediaSource = null;
+        if(chosenRecipes[i].media[0].includes('.mp4')){
+           mediaSource = 'videos/' + chosenRecipes[i].media[0];
+           videoContainers[i].children[1].children[0].children[0].setAttribute('src', chosenRecipes[i].media[0]);
+           videoContainers[i].children[1].children[0].children[1].setAttribute('style','display:none;');
+        }
+        else{
+           mediaSource = 'photos/' + chosenRecipes[i].media[0];
+           videoContainers[i].children[1].children[0].children[1].setAttribute('src', chosenRecipes[i].media[0]);
+           videoContainers[i].children[1].children[0].children[1].setAttribute('style','display:none;');
+        }
+        // videoContainers[i].children[1].children[0].children[0].setAttribute('src', chosenRecipes[i].media[0]);
+
     }
 }
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
+
+function findObjectInArray(array, object){
+    for(let i = 0; i < array.length; i++){
+        if (object == array[i]){
+            return i;
+        }
+    }
+    return -1;
+}
