76061cda9e3104a6ce4d9e63ccd09000953fb78e
BTran418 <btran418@gmail.com>
2019-03-31 22:17:21 -0600

    push

--- script/index_script.js
+++ script/index_script.js
@@ -4,6 +4,7 @@
  */
 
 let jsonObjects = null;
+let loginName = null;
 
 window.onload = function () {
 
@@ -25,25 +26,30 @@ window.onload = function () {
 function populateTrending(){
     let videosDiv = document.getElementById("video");
     let chosenRecipes = [];
-
+    let ids = [];
     //Selects 4 random recipes
     for(let i = 0; i < 4; i++){
-        let recipe = jsonObjects[getRandomInt(jsonObjects.length)];
+        ids.push(getRandomInt(jsonObjects.length));
+        console.log(ids);
+        let recipe = jsonObjects[ids[i]];
         //TODO: Wait for more than 4 recipes
         // while (findObjectInArray(chosenRecipes, recipe) == -1){
         //     recipe = jsonObjects[getRandomInt(jsonObjects.length)];
         // }
 
-        chosenRecipes.push(jsonObjects[getRandomInt(jsonObjects.length)]);
+        chosenRecipes.push(recipe);
     }
-    console.log(chosenRecipes)
+    // console.log(chosenRecipes)
+    // console.log(ids);
 
     //Get all video containers
     let videoContainers = document.getElementsByClassName('a_video');
+    
     for(let i = 0; i < videoContainers.length; i++){
+        //Set a href
+        videoContainers[i].parentElement.setAttribute('href', 'recipe.html?recipe-id=' + ids[i]);
         //Set name
         videoContainers[i].children[0].children[0].innerText = chosenRecipes[i].title;
-
         //Set media
         let mediaSource = null;
         if(chosenRecipes[i].media[0].includes('.mp4')){
@@ -51,7 +57,8 @@ function populateTrending(){
            videoContainers[i].children[1].children[0].children[0].setAttribute('src', mediaSource);
            videoContainers[i].children[1].children[0].children[1].children[0].setAttribute('style','display:none;');
            videoContainers[i].children[1].children[0].children[0].setAttribute('style','');
-
+           videoContainers[i].children[1].children[0].children[0].width = 320;
+           videoContainers[i].children[1].children[0].children[0].height = 180;
         }
         else{
            mediaSource = 'photos/' + chosenRecipes[i].media[0];
@@ -60,7 +67,6 @@ function populateTrending(){
            videoContainers[i].children[1].children[0].children[1].children[0].setAttribute('style','');
         }
         // videoContainers[i].children[1].children[0].children[0].setAttribute('src', chosenRecipes[i].media[0]);
-
     }
 }
 
@@ -75,4 +81,19 @@ function findObjectInArray(array, object){
         }
     }
     return -1;
+}
+
+function cancelLoginClicked(){
+    document.getElementsByClassName("login_part")[0].classList.add('disabled');
+}
+
+function loginBtnClicked(){
+    document.getElementsByClassName("login_part")[0].classList.remove('disabled');
+}
+
+function acceptLoginClicked(){
+    login = document.querySelector('.email_login input').value;
+    document.getElementsByClassName("login_part")[0].classList.add('disabled');
+    document.querySelector('#login').innerText = '@'+login;
+    document.querySelector('#login').setAttribute('onclick','');
 }
--- script/search.js
+++ script/search.js
@@ -2,7 +2,8 @@
 
 let jsonObjects = null;
 let results = null;
-
+let searchType = null;
+let ids = [];
 window.onload = function () {
 
     let request = new XMLHttpRequest();
@@ -13,52 +14,59 @@ window.onload = function () {
         jsonObjects = request.response; //Create recipe objects from a JSON file.
         // console.log(jsonObjects);
         // searchByText('oven');
-        
+        console.log(jsonObjects[4].tags);
+
     }
 }
 
-function searchByText(search){
+function searchByText(search) {
     results = [];
+    ids = [];
     let searchResults = [];
+    search = search.toLowerCase();
 
-    // for(let i = 0; i < jsonObjects.length; i++){
-    //     for(let j = 0; j < jsonObjects[i].bodies.length; i++){
-    //         if(jsonObjects[i].bodies[j].includes(search)){
-    //             searchResults.push(jsonObjects[i]);
-    //         }
-    //     }
-    // }
-    for(let i = 0; i < jsonObjects.length; i++){
-        if(jsonObjects[i]['description'].includes(search) || jsonObjects[i]['title'].includes(search)){
+    for (let i = 0; i < jsonObjects.length; i++) {
+        if (jsonObjects[i]['description'].toLowerCase().includes(search) || jsonObjects[i]['title'].toLowerCase().includes(search)) {
             searchResults.push(jsonObjects[i]);
+            ids.push(i);
         }
     }
 
     results = searchResults;
+    return searchResults;
 }
 
-function searchByTag(){
+function searchByTag(tagsSearch) {
     results = [];
-}
-
-function populateResults(recipe){
-    if(document.getElementById('result').children.length != 0){
-        for(let i = 0; i < document.getElementById('result').children.length; i++){
-            document.getElementById('result').children[i].remove();
+    ids = [];
+    for (let i = 0; i < tagsSearch.length; i++) { //Per tag
+        for (let j = 0; j < jsonObjects.length; j++) { //Per Recipe
+            for (let x = 0; x < jsonObjects[j]['tags'].length; x++) {
+                if (jsonObjects[j].tags[x].toLowerCase() == tagsSearch[i].toLowerCase()) {
+                    results.push(jsonObjects[j]);
+                    ids.push(j);
+                }
+            }
         }
     }
-    
+}
+
+function populateResults(recipe, id) {
+
+
     let individualResult = document.createElement('div');
     let media = document.createElement('div');
     let mediaSrc = null;
-    
-    if (recipe.media[0].includes(".mp4")){
+
+    if (recipe.media[0].includes(".mp4")) {
         mediaSrc = document.createElement('video');
-        mediaSrc.setAttribute('src','videos/' + recipe.media[0]);
-    }   
-    else{
+        mediaSrc.setAttribute('src', 'videos/' + recipe.media[0]);
+        mediaSrc.controls = true;
+
+    }
+    else {
         mediaSrc = document.createElement('img');
-        mediaSrc.setAttribute('src','photos/' + recipe.media[0]);
+        mediaSrc.setAttribute('src', 'photos/' + recipe.media[0]);
     }
 
     let description = document.createElement('div');
@@ -74,9 +82,14 @@ function populateResults(recipe){
     title.classList.add('recipe_title');
     text.classList.add('recipe_text');
 
-    mediaSrc.setAttribute('width','320');
+    mediaSrc.setAttribute('width', '320');
+    mediaSrc.setAttribute('height', '180');
 
-    description.append(title);
+    let aTitle = document.createElement('a');
+    aTitle.setAttribute('href','recipe.html?recipe-id='+ id);
+    aTitle.append(title);
+    description.append(aTitle);
+
     description.append(text);
     media.append(mediaSrc);
     individualResult.append(media);
@@ -85,15 +98,90 @@ function populateResults(recipe){
     document.getElementById('result').append(individualResult);
 }
 
-function searchBtnClicked(){
-    let search = document.getElementById('search_criteria').value;
-    console.log('asdasd');
-    if(search == '' || search == null){
-        return;
+function searchBtnClicked() {
+
+    //Remove previous results
+    if (document.getElementById('result').children.length != 0) {
+        while(document.getElementById('result').children.length != 0){
+            document.getElementById('result').children[0].remove();
+        }
+        // for (let i = 0; i < document.getElementById('result').children.length; i++) {
+        //     document.getElementById('result').children[i].remove();
+        // }
     }
 
-    searchByText(search);
-    for(let i = 0; i < results.length; i++){
-        populateResults(results[i]);
+    let searchType = document.querySelector('.search_type.active');
+    //Searcg by text
+    if (searchType.innerText == 'Text') {
+        //Dont do anything if the search box is empty.
+        let search = document.getElementById('search_criteria').value;
+        if (search == '' || search == null) {
+            return;
+        }
+        console.log(searchByText(search));
+        
+        // for (let i = 0; i < results.length; i++) {
+        //     populateResults(results[i]);
+        // }
+    }
+    //Search by tags
+    else if (searchType.innerText == 'Tags') {
+        let activeTagsDiv = document.querySelectorAll('.tag.active');
+        let temp = [];
+        for(let i = 0; i < activeTagsDiv.length; i++){
+            temp.push(activeTagsDiv[i].innerText);
+        }
+        console.log(searchByTag(temp));
+    }
+    for (let i = 0; i < results.length; i++) {
+        populateResults(results[i], ids[i]);
+    }
+    // searchByText(search);
+
+
+}
+
+/**
+ * Used to change the .active element and update the search type.
+ */
+function criteriaClicked(element) {
+    let active = document.querySelector('.search_type.active');
+    if (active == null) {
+        element.classList.add('active');
+    }
+    else {
+        active.classList.remove('active');
+        element.classList.add('active');
+    }
+
+    //Change elements
+    let textSearch = '<input type="text" name="search_box" id="search_criteria">' +
+        '<input class="btn" type="button" value="Search" onclick="searchBtnClicked()">';
+    let tagSearch = '<div class=\'tag_collection\'>' +
+        '<div class=\'tag\' onclick="tagClicked(this)">Cheese</div>' +
+        '<div class=\'tag\' onclick="tagClicked(this)">Original Recipe</div>' +
+        '<div class=\'tag\' onclick="tagClicked(this)">Quick</div>' +
+        '<div class=\'tag\' onclick="tagClicked(this)">10 People</div>' +
+        '<div class=\'tag\' onclick="tagClicked(this)">Dinner</div>' +
+        '<div class=\'tag\' onclick="tagClicked(this)">Breakfast</div>' +
+        '<div class=\'tag\' onclick="tagClicked(this)">Brunch</div>' +
+        '</div>' +
+        '<input class="btn" type="button" value="Search" onclick="searchBtnClicked()">';
+
+
+    if (element.innerText.includes('Text')) {
+        document.getElementsByClassName('context_search')[0].innerHTML = textSearch;
+    }
+    else {
+        document.getElementsByClassName('context_search')[0].innerHTML = tagSearch;
+    }
+}
+
+function tagClicked(element) {
+    if (element.classList.contains('active')) {
+        element.classList.remove('active');
+    }
+    else {
+        element.classList.add('active');
     }
 }
